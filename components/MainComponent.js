0ffd7af1986a46eba2f4c8da96110c1b308ff322
Ngo Nguyen Bang <bangmapleproject@gmail.com>
2020-10-27 20:32:56 +0700

    Finishing up basic navigation

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,114 +1,74 @@
 import React from 'react'
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
-import { SafeAreaView, View, Platform, Text } from 'react-native';
-import { createAppContainer } from 'react-navigation';
+import { View, Platform, SafeAreaView } from 'react-native';
 import { Constants } from 'react-native-unimodules';
 import Home from './HomeComponent';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import { createStackNavigator } from '@react-navigation/stack';
-import Test from './TestComponent';
 
+const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
-const NewMenuNavigator = () => {
+
+const MenuNavigator = () => {
     return (
-        <NavigationContainer>
             <Stack.Navigator initialRouteName="Menu"
-             screenOptions={{headerStyle: {
-                 backgroundColor: '#512DA8'
-             },
-             headerTintColor: '#fff',
-             headerTitleStyle: {
-                 color: '#fff'
-             }}}>
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: '#512DA8'
+                    },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        color: '#fff'
+                    }
+                }}>
                 <Stack.Screen name="Menu" component={Menu} />
                 <Stack.Screen name="Dishdetail" component={Dishdetail} />
             </Stack.Navigator>
-        </NavigationContainer>
     );
 }
-/*const MenuNavigator = createStackNavigator(
-    {
-        Menu: { screen: Menu },
-        Dishdetail: { screen: Dishdetail }
-    },
-    {
-        initialRouteName: 'Menu',
-        navigationOptions: {
+
+const HomeNavigator = () => {
+    return (
+        <Stack.Navigator screenOptions={{
             headerStyle: {
                 backgroundColor: '#512DA8'
             },
-            headerTintColor: '#fff',
             headerTitleStyle: {
                 color: '#fff'
-            }
-        }
-    }
-);*/
+            },
+            headerTintColor: '#fff'
+            }}>
+                <Stack.Screen name="Home" component={Home}/>
+        </Stack.Navigator>
+    );
+}
 
-/*const HomeNavigator = createStackNavigator({
-    Home: { screen: Home }
-}, {
-    navigationOptions: {
-        headerStyle: {
-            backgroundColor: '#512DA8'
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-            color: '#fff'
-        }
-    }
-});*/
-
-const Drawer = createDrawerNavigator();
-
-/*
-   <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-*/
-const NewMainNavigator = () => {
+const MainNavigator = () => {
     return (
         <NavigationContainer>
             <Drawer.Navigator
                 drawerStyle={{ backgroundColor: '#D1C4E9' }}>
-                <Drawer.Screen name='Home' options={{ drawerLabel: 'hi' }}
-                    component={Home} />
-                <Drawer.Screen name='Menu'
-                    component={Menu} />
+                <Drawer.Screen name='Home' options={{ 
+                    title: 'Home',
+                    drawerLabel: 'Home'
+                 }}
+                    component={HomeNavigator} />
+                <Drawer.Screen name='Menu' options={{
+                    title: 'Menu',
+                    drawerLabel: 'Menu'
+                }}
+                    component={MenuNavigator} />
             </Drawer.Navigator>
-        </NavigationContainer>);
+        </NavigationContainer>
+    );
 }
-
-/*const MainNavigator = createDrawerNavigator({
-    Home: {
-        screen: HomeNavigator,
-        navigationOptions: {
-            title: 'Home'
-        }
-    },
-    Menu: {
-        screen: MenuNavigator,
-        navigationOptions: {
-            title: 'Menu',
-            drawerLabel: 'Menu'
-        }
-    }
-}, {
-    drawerBackgroundColor: '#D1C4E9'
-})*/
-
-const AppMenuNavigator = createAppContainer(NewMenuNavigator)
-const AppMainNavigator = createAppContainer(NewMainNavigator);
 
 export default function Main() {
     return (
         <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
-            <NewMenuNavigator />
+            <MainNavigator />
         </View>
     );
 }
